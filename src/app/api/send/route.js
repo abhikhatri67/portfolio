8232d2a8e9856cb87cc3193a4c2bce8714c20e4b
Abhishek Khatri <abhikhatri67@gmail.com>
2024-06-15 22:20:12 +0530

    Removed email part for now.

--- src/app/api/send/route.js
+++ src/app/api/send/route.js
@@ -1,27 +1,27 @@
-// import { EmailTemplate } from "../../../components/EmailTemplate";
-import { Resend } from "resend";
+// // import { EmailTemplate } from "../../../components/EmailTemplate";
+// import { Resend } from "resend";
 
-const resend = new Resend(process.env.RESEND_API_KEY);
+// const resend = new Resend(process.env.RESEND_API_KEY);
 
-export async function POST() {
-  try {
-    const { data, error } = await resend.emails.send({
-      from: "Abhishek <abhikhatri67@gmail.com>",
-      to: ["abhikhatri67@gmail.com"],
-      subject: "Hello world",
-      react: (
-        <>
-          <p>Email Body</p>
-        </>
-      ),
-    });
+// export async function POST() {
+//   try {
+//     const { data, error } = await resend.emails.send({
+//       from: "Abhishek <abhikhatri67@gmail.com>",
+//       to: ["abhikhatri67@gmail.com"],
+//       subject: "Hello world",
+//       react: (
+//         <>
+//           <p>Email Body</p>
+//         </>
+//       ),
+//     });
 
-    if (error) {
-      return Response.json({ error }, { status: 500 });
-    }
+//     if (error) {
+//       return Response.json({ error }, { status: 500 });
+//     }
 
-    return Response.json(data);
-  } catch (error) {
-    return Response.json({ error }, { status: 500 });
-  }
-}
+//     return Response.json(data);
+//   } catch (error) {
+//     return Response.json({ error }, { status: 500 });
+//   }
+// }
